fbf6a72eb2e256b2639df4b926e684c73a2b1e08
Van Tran <van.tran@asnet.com.vn>
2025-02-25 17:15:54 +0700

    feat(native): enhance splash screen functionality and update app.json configuration

--- apps/native/app/index.tsx
+++ apps/native/app/index.tsx
@@ -1,8 +1,55 @@
 import { StyleSheet, Text, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { Button } from '@repo/ui';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback, useEffect, useState } from 'react';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+	duration: 1000,
+	fade: true
+});
 
 export default function Native() {
+	const [appIsReady, setAppIsReady] = useState(false);
+
+	useEffect(() => {
+		async function prepare() {
+			try {
+				// Pre-load fonts, make any API calls you need to do here
+				//   await Font.loadAsync(Entypo.font);
+				// Artificially delay for two seconds to simulate a slow loading
+				// experience. Remove this if you copy and paste the code!
+				await new Promise((resolve) => setTimeout(resolve, 2000));
+			} catch (e) {
+				console.warn(e);
+			} finally {
+				// Tell the application to render
+				setAppIsReady(true);
+			}
+		}
+
+		prepare();
+	}, []);
+
+	const onLayoutRootView = useCallback(() => {
+		if (appIsReady) {
+			// This tells the splash screen to hide immediately! If we call this after
+			// `setAppIsReady`, then we may see a blank screen while the app is
+			// loading its initial state and rendering its first pixels. So instead,
+			// we hide the splash screen once we know the root view has already
+			// performed layout.
+			SplashScreen.hide();
+		}
+	}, [appIsReady]);
+
+	if (!appIsReady) {
+		return null;
+	}
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.header}>Native</Text>
