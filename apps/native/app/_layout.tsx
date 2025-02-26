8b59c6f12eb9b2db5821376ca32df61219927ee4
Van Tran <van.tran@asnet.com.vn>
2025-02-26 14:57:38 +0700

    chore: add Prettier configuration and standardize code formatting across files

--- apps/native/app/_layout.tsx
+++ apps/native/app/_layout.tsx
@@ -2,11 +2,11 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import {
-	Poppins_400Regular,
-	Poppins_500Medium,
-	Poppins_700Bold,
-	Poppins_600SemiBold,
-	useFonts
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_600SemiBold,
+  useFonts,
 } from '@expo-google-fonts/poppins';
 
 // Keep the splash screen visible while we fetch resources
@@ -14,27 +14,27 @@ SplashScreen.preventAutoHideAsync();
 
 // Set the animation options. This is optional.
 SplashScreen.setOptions({
-	duration: 1000,
-	fade: true
+  duration: 1000,
+  fade: true,
 });
 
 const AppLayout = () => {
-	const [loaded, error] = useFonts({
-		Poppins_400Regular,
-		Poppins_500Medium,
-		Poppins_600SemiBold,
-		Poppins_700Bold
-	});
+  const [loaded, error] = useFonts({
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+    Poppins_700Bold,
+  });
 
-	useEffect(() => {
-		if (loaded) {
-			SplashScreen.hideAsync();
-		} else if (error) {
-			console.error('Error loading fonts:', error.message);
-		}
-	}, [loaded, error]);
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync();
+    } else if (error) {
+      console.error('Error loading fonts:', error.message);
+    }
+  }, [loaded, error]);
 
-	return <Stack />;
+  return <Stack />;
 };
 
 export default AppLayout;
